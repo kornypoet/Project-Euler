0ddde0c9c79147539c9d38e1573e4a6f9ce3a050
Travis Dempsey <dempsey.travis@gmail.com>
2010-10-28 10:26:53 -0500

    another one down

--- problem_12.rb
+++ problem_12.rb
@@ -1,8 +1,15 @@
-load 'math_fun.rb'
-x = 1
-y = x.lcm(x + 1)
-while x <= 500
-  y = y.lcm(x)
-  x +=1 
+y, x = 1, 1
+while true
+  factors = 0
+  x += 1
+  y += x
+  for z in (1..Math.sqrt(y)) do
+    if y % z == 0
+      factors += 2
+    end
+  end
+  if factors > 499
+    p y
+    break
+  end
 end
-p y
