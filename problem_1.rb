439eaa634395b8169fefc2fa1953d8eb64883159
Travis Dempsey <dempsey.travis@gmail.com>
2010-10-27 23:20:10 -0500

    syntax fix

--- problem_1.rb
+++ problem_1.rb
@@ -1,8 +1,8 @@
-x, running_total = 0
-
-for ( x < 1000)
-  if ( x % 3 || x % 5 == 0)
-    running_total += x
+running_total = 0
+x = (1..1000).to_a
+x.each do |num|
+  if ( num % 3 == 0 || num % 5 == 0)
+    running_total += num
   end
 end
 
